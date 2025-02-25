9b3c407d96a78ee95f0f0906dac208d8027bcd7e
Kevin Espinoza <kevin@dicalzeus.com>
2025-02-24 17:23:11 -0800

    Initial commit

---
+++ app/src/main/java/com/kevinespinoza/habitapp/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.kevinespinoza.habitapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.kevinespinoza.habitapp.ui.theme.HabitAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            HabitAppTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Text(text = "Hola!")
+                }
+            }
+        }
+    }
+}
